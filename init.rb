7e650bfff088f9197326ff34d5512ec8e61de307
chantra <chantr4@gmail.com>
2010-04-30 10:22:12 +0200

    Releasing 0.0.3

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :redmine_whining do
   name 'Redmine Whining plugin'
   author 'Emmanuel Bretelle'
   description 'A plugin to send email alerts when an issue did not get udated since a certain amount of time. This plugin MUST be called from a cronjob.'
-  version '0.0.2'
+  version '0.0.3'
 end
