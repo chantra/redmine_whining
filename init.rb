2497d290aaba9126d40a071f8bf1f16466d6c5dd
Farzad FARID <ffarid@pragmatic-source.com>
2010-07-08 23:20:19 +0800

    Add missing default 'trackers' setting, plugin cannot work without it

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_whining do
   description 'A plugin to send email alerts when an issue did not get udated since a certain amount of time. This plugin MUST be called from a cronjob.'
   version '0.0.3'
 
-  settings :default => { :delay_default => 7 }, :partial => 'settings/whining_settings'
+  settings :default => { :delay_default => 7, :trackers => [] }, :partial => 'settings/whining_settings'
 
   project_module :whining do
     # we need a dummy permission to enable per-project module enablement
