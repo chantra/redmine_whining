8ec40392274890d45925bafdae99e74341f2bf51
chantra <chantra@debuntu.org>
2010-08-17 13:47:15 +0200

    Bumping to v0.0.4
    Merging changes from stable 0.0.X branch to master

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_whining do
   name 'Redmine Whining plugin'
   author 'Emmanuel Bretelle'
   description 'A plugin to send email alerts when an issue did not get udated since a certain amount of time. This plugin MUST be called from a cronjob.'
-  version '0.0.3'
+  version '0.0.4'
 
   settings :default => { :delay_default => 7, :trackers => [] }, :partial => 'settings/whining_settings'
 
@@ -12,4 +12,5 @@ Redmine::Plugin.register :redmine_whining do
     # we need a dummy permission to enable per-project module enablement
     permission :dummy, {:dummy => [:dummy]}, :public => true
   end
+  version '0.0.4'
 end
