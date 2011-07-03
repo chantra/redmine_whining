24cf6e848081a5087f7d5a8f0ec4fe9a7f2bb223
chantra <chantra@debuntu.org>
2011-07-04 00:43:04 +0200

    [init.rb] Adding URL info

--- init.rb
+++ init.rb
@@ -3,6 +3,9 @@ require 'redmine'
 Redmine::Plugin.register :redmine_whining do
   name 'Redmine Whining plugin'
   author 'Emmanuel Bretelle'
+  author_url 'http://www.debuntu.org'
+  url 'http://redmine.debuntu.org/projects/redmine-whining'
+
   description 'A plugin to send email alerts when an issue did not get udated since a certain amount of time. This plugin MUST be called from a cronjob.'
   version '0.0.5'
 end
