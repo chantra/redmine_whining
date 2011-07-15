ff6d5d1bf7aa4a4456840873b72d820c18095678
chantra <chantra@debuntu.org>
2011-07-15 02:17:13 +0200

    Bumping to version 0.0.6

--- init.rb
+++ init.rb
@@ -7,5 +7,5 @@ Redmine::Plugin.register :redmine_whining do
   url 'http://redmine.debuntu.org/projects/redmine-whining'
 
   description 'A plugin to send email alerts when an issue did not get udated since a certain amount of time. This plugin MUST be called from a cronjob.'
-  version '0.0.5'
+  version '0.0.6'
 end
